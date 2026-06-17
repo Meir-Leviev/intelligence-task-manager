8ff13e99ee5b69e51cf8832971001594bf430eea
Meir Leviev <meirleviev@gmail.com>
2026-06-17 14:57:34 +0300

    Add update_agent() and deactivate_agent()

--- database/agent_db.py
+++ database/agent_db.py
@@ -3,7 +3,7 @@ from db_connection import DBConnection as db
 
 class AgentDB:
     def create_agent(self, data: dict):
-        allowed_keys = ['name', 'specialty', 'agent_rank']
+        allowed_keys = ["name", "specialty", "agent_rank"]
         keys = [k for k in data.keys() if k in allowed_keys]
         keys_str = ", ".join(keys)
         values = [v for v in data.values()]
@@ -23,7 +23,7 @@ class AgentDB:
                 cursor.execute("SELECT * FROM agents")
                 agents = cursor.fetchall()
         return agents
-    
+
     def get_agent_by_id(self, id):
         with db().get_connection() as conn:
             with conn.cursor(dictionary=True) as cursor:
@@ -32,10 +32,34 @@ class AgentDB:
         return agent
 
     def update_agent(self, id, data):
-        pass
+        allowed_keys = [
+            "name",
+            "specialty",
+            "agent_rank",
+            "is_active",
+            "completed_missions",
+            "failed_missions",
+            "agent_rank",
+        ]
+        keys = [f"{k}=%s" for k in data.keys() if k in allowed_keys]
+        keys_str = ", ".join(keys)
+        values = [v for v in data.values()] + [id]
+        sql = f"UPDATE agents SET {keys_str} WHERE id = %s"
+        with db().get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql, values)
+                is_success = cursor.rowcount > 0
+                conn.commit()
+        return is_success
 
     def deactivate_agent(self, id):
-        pass
+        sql = f"UPDATE agents SET is_active = FALSE WHERE id = %s"
+        with db().get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql, (id,))
+                is_success = cursor.rowcount > 0
+                conn.commit()
+        return is_success
 
     def increment_completed(self, id):
         pass
@@ -50,11 +74,10 @@ class AgentDB:
         pass
 
 
-data = {
-    'name': "meir", 
-    'specialty': 'python',
-    'agent_rank': 'Junior'
-}
 
-a = AgentDB().create_agent(data)
-print(a)
+if __name__ == "__main__":
+
+    data = {"name": "Moshe", "specialty": "python", "agent_rank": "Senior", }
+
+    a = AgentDB().update_agent(1,data )
+    print(a)
